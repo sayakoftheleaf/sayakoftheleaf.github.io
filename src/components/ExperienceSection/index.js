d1fd3f51bf75e00703c34905151f1a13872de32a
sayakoftheleaf <sayakchatterjee1309@gmail.com>
2018-10-01 00:32:03 -0400

    updated entries

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -17,14 +17,14 @@ class ExperienceSection extends React.Component {
             title='Yogi Network'
             link='https://www.camyogi.in/'
             timeperiod='July 2018 - Present'
-            subtitle='Built an Admin tool and database management system (from scratch) based on PostgreSQL, NodeJS, ElectronJS, and React.'
+            subtitle='Built an Admin tool and database management system (from scratch) based on MongoDB, NodeJS, ReactJS, ElectronJS and Mongoose. The application is a GUI implementation of a lot of functionalities provided by MongooseJS.'
           />
           <ExperienceUnit
             logo={hriLogo}
             colour='#FFFFFF'
             title='Tufts HRI Lab'
             link='https://hrilab.tufts.edu/'
-            timeperiod='2016'
+            timeperiod='June 2016 - August 2016'
             subtitle="Worked on the NASA space rover challenge, on testing and improving the rover's mapping and navigating capabilities on 3-d unmapped enviornments (simulated planets with auto-generated random terrain)."
           />
           <ExperienceUnit
@@ -32,7 +32,7 @@ class ExperienceSection extends React.Component {
             colour='#FFFFFF'
             title='Tufts HCI Lab'
             link='http://hci.cs.tufts.edu/'
-            timeperiod='2017 - 2018'
+            timeperiod='June 2017 - August 2017'
             subtitle="Worked on an developing the front and back end software for experiement that sent electrical impulses to test subjects when their eye movements indicated that they were about to lose concentration during reading comprehension."
           />
         </div>
